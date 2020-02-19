153b2b6e06cd0c9ea3c9973a7798d099b520249c
Rob <rob@itsallinthecode.com>
2020-02-19 18:12:33 +0000

    Change excerpt tag

--- src/components/Post.js
+++ src/components/Post.js
@@ -14,9 +14,9 @@ const Post = props => (
           <Card.Title
             dangerouslySetInnerHTML={{ __html: props.title }}
           ></Card.Title>
-          <Card.Text
-            dangerouslySetInnerHTML={{ __html: props.excerpt }}
-          ></Card.Text>
+          <Card.Text>
+            <div dangerouslySetInnerHTML={{ __html: props.excerpt }}></div>
+          </Card.Text>
           <Button variant="warning" href={props.readMore}>
             Read more...
           </Button>
